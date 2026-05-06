d013a196e1c50dd67c61337815edbf5c2353834f
Shruti Garg <shrutigarg@Shrutis-MacBook-Air.local>
2026-05-06 23:11:07 +0530

    auth

---
+++ src/main/java/DistributedTaskQueueSystem/demo/Config/JwUtil.java
@@ -0,0 +1,50 @@
+package DistributedTaskQueueSystem.demo.Config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwUtil {
+    
+    private static final String SECRET_KEY = "mysecretkey12345"; //
+    private static final long EXPIRATION_TIME = 86400000; // 24 hours in milliseconds
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
+    public String generateToken(String email, String role) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+    public String extractEmail(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            
+            return !getClaims(token).getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+}
---
+++ src/main/java/DistributedTaskQueueSystem/demo/Model/DTO/AuthResponse.java
@@ -0,0 +1,12 @@
+package DistributedTaskQueueSystem.demo.Model.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+    private String email;
+    private String role;
+}
